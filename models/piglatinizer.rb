37d1951a1fb4ab5f70668d311aaac8f13fb8b2ea
TylerNodell <Tyler.l.nodell@gmail.com>
2018-08-06 17:25:59 -0400

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,59 +1,59 @@
-# class PigLatinizer
-#   attr_accessor :text
-#   def initialize(text)
-#     @text = text.downcase
-#   end
+class PigLatinizer
+  attr_accessor :text
+  def initialize(text)
+    @text = text.downcase
+  end
 
-#   def latinizer
-#     split_text = @text.split(" ")
-#     latin = split_text.map do |word|
-#       front = word.slice(/^.*(?=[aeiou])/) + 'ay'
-#       word.slice!(/^.*(?=[aeiou])/)
-#       word + front
-#     end
-#     latin.join(' ')
-#   end
-# end
+  def latinizer
+    split_text = @text.split(" ")
+    latin = split_text.map do |word|
+      front = word.slice(/^.*(?=[aeiou])/) + 'ay'
+      word.slice!(/^.*(?=[aeiou])/)
+      word + front
+    end
+    latin.join(' ')
+  end
+end
 
 # Above works but it wouldnt pass learn
 
 
-class PigLatinizer
+# class PigLatinizer
 
-  def piglatinize(input_str)
-    x = (input_str.split(" ").length == 1) ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
-    puts x
-    x
-  end
+#   def piglatinize(input_str)
+#     x = (input_str.split(" ").length == 1) ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
+#     puts x
+#     x
+#   end
 
-  private
+#   private
 
-  def consonant?(char)
-    !char.match(/[aAeEiIoOuU]/)
-  end
+#   def consonant?(char)
+#     !char.match(/[aAeEiIoOuU]/)
+#   end
 
-  def piglatinize_word(word)
-    # word starts with vowel
-    if !consonant?(word[0])
-      word = word + "w"
-    # word starts with 3 consonants
-    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
-      word = word.slice(3..-1) + word.slice(0,3)
-    # word starts with 2 consonants
-    elsif consonant?(word[0]) && consonant?(word[1])
-      word = word.slice(2..-1) + word.slice(0,2)
-    # word starts with 1 consonant
-    else
-      word = word.slice(1..-1) + word.slice(0)
-    end
-    word << "ay"
-  end
+#   def piglatinize_word(word)
+#     # word starts with vowel
+#     if !consonant?(word[0])
+#       word = word + "w"
+#     # word starts with 3 consonants
+#     elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+#       word = word.slice(3..-1) + word.slice(0,3)
+#     # word starts with 2 consonants
+#     elsif consonant?(word[0]) && consonant?(word[1])
+#       word = word.slice(2..-1) + word.slice(0,2)
+#     # word starts with 1 consonant
+#     else
+#       word = word.slice(1..-1) + word.slice(0)
+#     end
+#     word << "ay"
+#   end
 
-  def piglatinize_sentence(sentence)
-    sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
-  end
-
-
+#   def piglatinize_sentence(sentence)
+#     sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
+#   end
 
 
-end
+
+
+# end
