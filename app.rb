f9645cda26e205d46242e90ab24e90f4d2a450a5
TylerNodell <Tyler.l.nodell@gmail.com>
2018-08-06 17:14:53 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,31 @@
+# require_relative 'config/environment'
+
+# class App < Sinatra::Base
+#   get '/' do
+#     erb :user_input
+#   end
+
+#   post '/piglatinize' do
+#     @text_input = PigLatinizer.new(params[:user_phrase])
+
+#     erb :results
+#   end
+# end
+
+# above works but doesnt pass learn
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    pl = PigLatinizer.new
+    @piglatin = pl.piglatinize(params[:user_phrase])
+    erb :results
+  end
+
 end
