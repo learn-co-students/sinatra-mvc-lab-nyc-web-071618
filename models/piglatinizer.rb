264f93d29ccc462ce53658271b5d247502032ec4
chejianchao <820992376@qq.com>
2018-08-07 17:12:56 -0400

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,24 @@
+
+class PigLatinizer
+  def initialize()
+  end
+
+  def piglatinize(str)
+    str_list = str.split(" ")
+    vowels = "aoiueAOIUE"
+    str_list.map{|str|
+      idx = 0
+      while idx <str.size
+        break if vowels.include?(str[idx])
+        idx += 1
+      end
+
+      new_str = str[idx...str.size] + str[0...idx]
+      if idx == 0
+        new_str += "way"
+      else
+        new_str += "ay"
+      end
+    }.join(" ")
+  end
+end
